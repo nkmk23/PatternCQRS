5e49bb958a60fd088e9a79064c7310d700bbbe6d
nkmk23 <nsnicolassr@hotmail.com>
2024-07-01 10:23:53 -0500

    build: Eliminar paquetes

---
+++ Customer.API/Controllers/CustomerController.cs
@@ -0,0 +1,60 @@
+﻿using Customer.Application.Commands;
+using Customer.Application.Handlers;
+using Customer.Application.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Customer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly CreateCustomerCommandHandler _createHandler;
+        private readonly DeleteCustomerCommandHandler _deleteHandler;
+        private readonly UpdateCustomerCommandHandler _updateHandler;
+        private readonly GetCustomerByIdQueryHandler _getHandler;
+
+        public CustomerController(
+            CreateCustomerCommandHandler createHandler,
+            DeleteCustomerCommandHandler deleteHandler,
+            UpdateCustomerCommandHandler updateHandler,
+            GetCustomerByIdQueryHandler getHandler)
+        {
+            _createHandler = createHandler;
+            _deleteHandler = deleteHandler;
+            _updateHandler = updateHandler;
+            _getHandler = getHandler;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCustomer(CreateCustomerCommand command)
+        {
+            var customerId = await _createHandler.Handle(command);
+            return Ok(customerId);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomer(int id)
+        {
+            var result = await _deleteHandler.Handle(new DeleteCustomerCommand { CustomerId = id });
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCustomer(int id, UpdateCustomerCommand command)
+        {
+            if (id != command.CustomerId)
+                return BadRequest();
+
+            var result = await _updateHandler.Handle(command);
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCustomerById(int id)
+        {
+            var customer = await _getHandler.Handle(new GetCustomerByIdQuery { CustomerId = id });
+            return Ok(customer);
+        }
+    }
+}
--- Customer.API/Program.cs
+++ Customer.API/Program.cs
@@ -1,8 +1,24 @@
+using Customer.Application.Handlers;
+using Customer.Domain.Interfaces.Infrastructure;
+using Customer.Infrastructure.Data;
+using Customer.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>(provider =>
+    new CustomerRepository(provider.GetRequiredService<AppDbContext>(), builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddTransient<CreateCustomerCommandHandler>();
+builder.Services.AddTransient<DeleteCustomerCommandHandler>();
+builder.Services.AddTransient<UpdateCustomerCommandHandler>();
+builder.Services.AddTransient<GetCustomerByIdQueryHandler>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
