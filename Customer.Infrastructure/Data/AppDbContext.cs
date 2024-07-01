580c7f6f05a95c8fdc99ca8e8aad08a34b2b1368
nkmk23 <nsnicolassr@hotmail.com>
2024-07-01 09:12:48 -0500

    feat: Configurar EntityFramework en el proyecto.

---
+++ Customer.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Customer.Domain.Entities.Customer;
+using Microsoft.EntityFrameworkCore;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace Customer.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<CustomerDTO> Customers { get; set; }
+    }
+}
