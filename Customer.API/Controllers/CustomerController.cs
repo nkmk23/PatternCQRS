5043a4d15af6fcdbd0fbde41f5fd36f68ed3e89c
nkmk23 <nsnicolassr@hotmail.com>
2024-07-02 08:08:50 -0500

    feat: agregar objeto para manejar la mensajeria y excepciones.

--- Customer.API/Controllers/CustomerController.cs
+++ Customer.API/Controllers/CustomerController.cs
@@ -1,7 +1,11 @@
 ﻿using Customer.Application.Commands;
 using Customer.Application.Handlers;
 using Customer.Application.Queries;
+using Customer.Domain.Entities.Customer;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Swashbuckle.AspNetCore.Annotations;
+using Customer.Domain.Entities.Customer.OUTPUT;
 
 namespace Customer.API.Controllers
 {
@@ -30,41 +34,133 @@ namespace Customer.API.Controllers
         }
 
         [HttpPost]
+        [SwaggerResponse(statusCode: 200, type: typeof(BodyResponse<int>), description: "Customer created successfully")]
+        [SwaggerResponse(statusCode: 400, type: typeof(BodyResponse<string>), description: "Bad input parameter")]
+        [SwaggerResponse(statusCode: 500, type: typeof(BodyResponse<string>), description: "Internal Error")]
         public async Task<IActionResult> CreateCustomer(CreateCustomerCommand command)
         {
-            var customerId = await _createHandler.Handle(command);
-            return Ok(customerId);
+            var response = new BodyResponse<int>();
+            try
+            {
+                var customerId = await _createHandler.Handle(command);
+                response.StatusCode = "200";
+                response.StatusDesc = "Customer created successfully";
+                response.Data = customerId;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = "500";
+                response.StatusDesc = "Internal Error";
+                return StatusCode(500, response);
+            }
         }
 
         [HttpDelete("{id}")]
+        [SwaggerResponse(statusCode: 200, type: typeof(BodyResponse<int>), description: "Customer deleted successfully")]
+        [SwaggerResponse(statusCode: 400, type: typeof(BodyResponse<string>), description: "Bad input parameter")]
+        [SwaggerResponse(statusCode: 500, type: typeof(BodyResponse<string>), description: "Internal Error")]
         public async Task<IActionResult> DeleteCustomer(int id)
         {
-            var result = await _deleteHandler.Handle(new DeleteCustomerCommand { CustomerId = id });
-            return Ok(result);
+            var response = new BodyResponse<int>();
+            try
+            {
+                var result = await _deleteHandler.Handle(new DeleteCustomerCommand { CustomerId = id });
+                response.StatusCode = "200";
+                response.StatusDesc = "Customer deleted successfully";
+                response.Data = result;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = "500";
+                response.StatusDesc = "Internal Error";
+                return StatusCode(500, response);
+            }
         }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateCustomer(int id, UpdateCustomerCommand command)
+        [SwaggerResponse(statusCode: 200, type: typeof(BodyResponse<int>), description: "Customer updated successfully")]
+        [SwaggerResponse(statusCode: 400, type: typeof(BodyResponse<string>), description: "Bad input parameter")]
+        [SwaggerResponse(statusCode: 500, type: typeof(BodyResponse<string>), description: "Internal Error")]
+        public async Task<IActionResult> UpdateCustomer(int id, [FromBody] UpdateCustomerCommand command)
         {
-            if (id != command.CustomerId)
-                return BadRequest();
+            var response = new BodyResponse<int>();
+            try
+            {
+                if (id != command.CustomerId)
+                {
+                    response.StatusCode = "400";
+                    response.StatusDesc = "Bad input parameter";
+                    return BadRequest(response);
+                }
 
-            var result = await _updateHandler.Handle(command);
-            return Ok(result);
+                var result = await _updateHandler.Handle(command);
+                response.StatusCode = "200";
+                response.StatusDesc = "Customer updated successfully";
+                response.Data = result;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = "500";
+                response.StatusDesc = "Internal Error";
+                return StatusCode(500, response);
+            }
         }
 
         [HttpGet("{id}")]
+        [SwaggerResponse(statusCode: 200, type: typeof(BodyResponse<CustomerDTO>), description: "Customer found")]
+        [SwaggerResponse(statusCode: 400, type: typeof(BodyResponse<string>), description: "Bad input parameter")]
+        [SwaggerResponse(statusCode: 404, type: typeof(BodyResponse<string>), description: "Customer not found")]
+        [SwaggerResponse(statusCode: 500, type: typeof(BodyResponse<string>), description: "Internal Error")]
         public async Task<IActionResult> GetCustomerById(int id)
         {
-            var customer = await _getHandler.Handle(new GetCustomerByIdQuery { CustomerId = id });
-            return Ok(customer);
+            var response = new BodyResponse<CustomerDTO>();
+            try
+            {
+                var customer = await _getHandler.Handle(new GetCustomerByIdQuery { CustomerId = id });
+                if (customer == null)
+                {
+                    response.StatusCode = "404";
+                    response.StatusDesc = "Customer not found";
+                    return NotFound(response);
+                }
+
+                response.StatusCode = "200";
+                response.StatusDesc = "Customer found";
+                response.Data = customer;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = "500";
+                response.StatusDesc = "Internal Error";
+                return StatusCode(500, response);
+            }
         }
 
         [HttpGet]
+        [SwaggerResponse(statusCode: 200, type: typeof(BodyResponse<List<CustomerDTO>>), description: "Customers found")]
+        [SwaggerResponse(statusCode: 400, type: typeof(BodyResponse<string>), description: "Bad input parameter")]
+        [SwaggerResponse(statusCode: 500, type: typeof(BodyResponse<string>), description: "Internal Error")]
         public async Task<IActionResult> GetAllCustomers()
         {
-            var customers = await _getAllHandler.Handle();
-            return Ok(customers);
+            var response = new BodyResponse<List<CustomerDTO>>();
+            try
+            {
+                var customers = await _getAllHandler.Handle();
+                response.StatusCode = "200";
+                response.StatusDesc = "Customers found";
+                response.Data = customers;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = "500";
+                response.StatusDesc = "Internal Error";
+                return StatusCode(500, response);
+            }
         }
     }
 }
---
+++ Customer.Domain/Entities/Customer/OUTPUT/BodyResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer.Domain.Entities.Customer.OUTPUT
+{
+    public class BodyResponse<T>
+    {
+        public string StatusCode { get; set; }
+        public string StatusDesc { get; set; }
+        public T Data { get; set; }
+    }
+}
